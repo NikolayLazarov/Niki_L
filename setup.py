3f8408db60340262b8f6d5b22c352da3c3024af0
NikolayLazarov <48582131+NikolayLazarov@users.noreply.github.com>
2020-03-29 23:33:00 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import  setup
+
+setup (name = 'elsys',
+		version = '1.0.0',
+		Descryption = 'A task for skript',
+		url='https://github.com/NikolayLazarov/Niki_L.git',
+		author = 'Nikolay Lazarov',
+		auhot_email = 'nikilazt7@gmail.com',
+		license = 'MIT',
+		packages = ['elsys'],
+		zip_safe = False
+
+)
